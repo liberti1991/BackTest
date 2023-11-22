43c936b2fa52de87aa337c31e04d13cd9d77c379
Rodrigo Liberti <liberti1991@gmail.com>
2023-11-22 10:46:27 -0300

    init server

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { server } from "./server/Server";
+
+
+
+server.listen(5555, () => console.log("Matheus ta com o cacete-te no cú"))
---
+++ src/server/Server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const server = express();
+
+server.get('/', (request, response) => {
+  return response.send('Hello Dev!')
+});
+
+export { server }
